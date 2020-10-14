5505750ecce54a99e870de6f48ce7cf3d3b94e32
CorbenG <61744834+CorbenG@users.noreply.github.com>
2020-10-14 14:32:45 -0400

    Slight Changes

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.LookAt(Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.up
+    }
+}
